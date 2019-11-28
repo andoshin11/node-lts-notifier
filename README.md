12d63fa8601440162c437415c7b3ca317cd27817
andoshin11 <shinglish11@gmail.com>
2019-11-28 16:33:19 +0900

    Update project

---
+++ README.md
@@ -0,0 +1,1 @@
+![](https://github.com/andoshin11/node-lts-notifier/workflows/.github/workflows/main.yml/badge.svg)
--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
 import { setOutput, setFailed } from '@actions/core'
 import fetch from 'node-fetch'
+import { parse } from 'fast-xml-parser'
+
+interface ReleaseInfo {
+  rss: {
+    channel: {
+      item: {
+        title: string
+      }[]
+    }
+  }
+}
 
 async function main() {
   try {
     console.log('Start script...')
     const res = await fetch('https://nodejs.org/ja/feed/releases.xml')
     const text = await res.text()
-    console.log(text)
-    setOutput('version', 'test-version')
+
+    const parsed: ReleaseInfo = parse(text, {
+      ignoreAttributes: false
+    })
+
+    const { item } = parsed.rss.channel
+    const versions = item.map(i => i.title)
+    const ltsVersions = versions
+      .map(v => v.match(/^Node v(\d+\.+\d+\.+\d) \(LTS\)/))
+      .filter(Boolean)
+      .map(i => i![1])
+
+    console.log(versions)
+    console.log(ltsVersions)
+    setOutput('version', ltsVersions.join(', '))
   } catch (e) {
     setFailed(e.message)
   }
