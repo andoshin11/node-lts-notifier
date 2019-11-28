2191af2b102f2efdc6846a6a4202e1e087ae1448
andoshin11 <shinglish11@gmail.com>
2019-11-28 15:58:18 +0900

    Update script

--- index.ts
+++ index.ts
@@ -5,9 +5,7 @@ async function main() {
   try {
     console.log('Start script...')
     const res = await fetch('https://nodejs.org/ja/feed/releases.xml')
-    const json = await res.json()
     const text = await res.text()
-    console.log(json)
     console.log(text)
     setOutput('version', 'test-version')
   } catch (e) {
